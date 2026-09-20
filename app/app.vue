e357695b162220958ca349b834fa840fb655a4f9
JT <Hawxy@users.noreply.github.com>
2026-09-20 19:10:26 +0800

    Update favicon

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,9 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', href: '/favicon.ico', sizes: '32x32' },
+    { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' },
+    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }
   ],
   htmlAttrs: {
     lang: 'en'
